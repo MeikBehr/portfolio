bf4f6a620239cccf8fad8f440085007fbd1d0e0d
Meik Behr <meik.behr@gmx.de>
2025-07-03 21:08:42 +0200

    feat: add initial folder structure and base components

---
+++ src/app/shared/mobile-overlay/mobile-overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-overlay',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-overlay.component.html',
+  styleUrl: './mobile-overlay.component.scss'
+})
+export class MobileOverlayComponent {
+
+}
---
+++ src/app/shared/success-msg/success-msg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-msg',
+  standalone: true,
+  imports: [],
+  templateUrl: './success-msg.component.html',
+  styleUrl: './success-msg.component.scss'
+})
+export class SuccessMsgComponent {
+
+}
