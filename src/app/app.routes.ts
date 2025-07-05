ad800ab8bfa6baad6e98ea05a94e92f41bc9635c
Meik Behr <meik.behr@gmx.de>
2025-07-05 18:43:22 +0200

    feat: add favicon, implement imprint and privacy-policy components, update header navigation and routing, fix SCSS import paths, and improve i18n files (de/en)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AtfComponent } from './main-content/atf/atf.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { ContactComponent } from './main-content/contact/contact.component';
+import { ProjectsComponent } from './main-content/projects/projects.component';
+import { SkillSetComponent } from './main-content/skill-set/skill-set.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './main-content/privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: AtfComponent},
+    { path: 'about-me', component: AboutMeComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'projects', component: ProjectsComponent},
+    { path: 'skill-set', component: SkillSetComponent},
+    { path: 'imprint', component: ImprintComponent},
+    { path: 'privacy-policy', component: PrivacyPolicyComponent}
+];
